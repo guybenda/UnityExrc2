d88f09b44d109dd3353cef771454e298199c9343
guybenda <email@example.com>
2022-05-23 18:24:05 +0300

    bestest i say

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+using UnityEngine.AI;
+using System.Collections;
+
 enum EnemyState
 {
     idle,
@@ -13,20 +16,37 @@ public class EnemyScript : MonoBehaviour
     int playerNoticing = 0;
     public int playerNoticeTreshold = 60;
 
+    public Transform[] points;
+
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        agent = GetComponent<NavMeshAgent>();
+
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
+        agent.autoBraking = false;
+
+        GotoNextPoint();
     }
 
     // Update is called once per frame
     void Update()
     {
-       //Physics.Raycast() 
+        //Physics.Raycast() 
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
     }
 
     void FixedUpdate()
-    {
+    {/*
         switch (state)
         {
             case EnemyState.idle:
@@ -39,11 +59,26 @@ public class EnemyScript : MonoBehaviour
             case EnemyState.attacking:
                 //TODO
                 break;
-        }
+        }*/
     }
 
     bool TryFindPlayer()
     {
         return false; // TODO
+    }
+
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
     }
 }
